c895244a9be62baf0b98e27b6f0d22fee98a792c
Deiv <77246331+Deivid-jmz@users.noreply.github.com>
2021-07-21 01:22:05 +0200

    Improve the tds today component

--- src/components/interfaces/NewTaskInterface/index.js
+++ src/components/interfaces/NewTaskInterface/index.js
@@ -5,6 +5,10 @@ const NewTaskInterface = () => {
 		<div className={styles.wrapper}>
 			<h2>New Todoist Task</h2>
 			Creates a new Todoist task in your inbox for today.
+			<ul>
+				<li>Use !!number to indicate priority</li>
+				<li>Use :: to separate task name from description</li>
+			</ul>
 		</div>
 	);
 };
