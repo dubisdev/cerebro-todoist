4982c5c12c2f67058518b944ed459a6f5c05a940
Deiv <77246331+Deivid-jmz@users.noreply.github.com>
2021-07-30 21:28:01 +0200

    Add translations

--- src/settings.js
+++ src/settings.js
@@ -1,26 +1,27 @@
-import strings from "./lang";
+import lang from "./lang";
+const strings = lang.settings;
+
 // ----------------- Plugin settings --------------------- //
 export const name = "Todoist Workflow";
 export const keyword = "tds";
 
-const s_settings = strings.settings;
 export const settings = {
 	"API Token": {
 		type: "string",
 		defaultValue: "",
-		description: s_settings.description,
+		description: strings.descriptionToken,
 	},
 
 	"New Task Command": {
 		type: "string",
 		defaultValue: "new",
-		description: "Customize the command for creating new tasks.",
+		description: strings.descriptionNewCommand,
 	},
 
 	"Today Tasks Command": {
 		type: "string",
 		defaultValue: "today",
-		description: "Customize the command for showing today tasks.",
+		description: strings.descriptionTodayCommand,
 	},
 };
 // ----------------- END Plugin settings --------------------- //
