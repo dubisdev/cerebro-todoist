0d03659b4dc261afc62be1c6d26f88f21d49835a
Deiv <77246331+Deivid-jmz@users.noreply.github.com>
2021-08-19 17:02:30 +0200

    Transaltion: french

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,12 +1,17 @@
 import es from "./es.json";
 import en from "./en.json";
+import fr from "./fr.json";
 
 //get the lang from the browser
 const lang = navigator.language;
 
 const langObject = {
 	es,
+	"es-ES": es,
 	en,
+	"en-US": en,
+	fr,
+	"fr-FR": fr,
 };
 
 export default langObject[lang] || en;
