dab93383e9ad776215fafaf1cc20dd0340e9bd9e
Deiv <77246331+Deivid-jmz@users.noreply.github.com>
2021-09-04 12:48:08 +0200

    feat: remove Error reporting

--- src/core-engine/index.js
+++ src/core-engine/index.js
@@ -1,11 +1,5 @@
-import configureErrorReporting from "./errorReporting";
 import { createTask } from "./taskServices";
 import updateChecker from "./updateChecker";
 import startPageAfterUpdate from "./startPageAfterUpdate";
 
-export {
-	configureErrorReporting,
-	createTask,
-	updateChecker,
-	startPageAfterUpdate,
-};
+export { createTask, updateChecker, startPageAfterUpdate };
