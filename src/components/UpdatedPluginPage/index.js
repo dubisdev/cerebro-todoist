85d1060c78444f2dca88a8512d1bf013feeb0342
dubisdev <dubisdev@gmail.com>
2022-04-16 21:20:54 +0200

    fix: changelog link

--- src/components/UpdatedPluginPage/index.js
+++ src/components/UpdatedPluginPage/index.js
@@ -5,30 +5,30 @@ import lang from "../../lang";
 const strings = lang.afterUpdate;
 
 const updatedPlugin = ({ actions, config }) => {
-	const handleClick = (action) => {
-		action === "view"
-			? actions.open("https://cerebro-todoist.dubis.dev/changelog")
-			: null;
-		setFirstUpdateStartToFalse(config);
-		actions.hideWindow();
-	};
+  const handleClick = (action) => {
+    action === "view"
+      ? actions.open("https://github.com/dubisdev/cerebro-todoist/releases")
+      : null;
+    setFirstUpdateStartToFalse(config);
+    actions.hideWindow();
+  };
 
-	return (
-		<div className={styles.wrapper}>
-			<h1>{strings.pluginUpdated}</h1>
-			<KeyboardNav>
-				<ul className={styles.list}>
-					<KeyboardNavItem onSelect={() => handleClick("view")}>
-						{strings.viewChangelog}
-					</KeyboardNavItem>
+  return (
+    <div className={styles.wrapper}>
+      <h1>{strings.pluginUpdated}</h1>
+      <KeyboardNav>
+        <ul className={styles.list}>
+          <KeyboardNavItem onSelect={() => handleClick("view")}>
+            {strings.viewChangelog}
+          </KeyboardNavItem>
 
-					<KeyboardNavItem onSelect={() => handleClick("close")}>
-						{strings.clickToHideWindow}
-					</KeyboardNavItem>
-				</ul>
-			</KeyboardNav>
-		</div>
-	);
+          <KeyboardNavItem onSelect={() => handleClick("close")}>
+            {strings.clickToHideWindow}
+          </KeyboardNavItem>
+        </ul>
+      </KeyboardNav>
+    </div>
+  );
 };
 
 export default updatedPlugin;
