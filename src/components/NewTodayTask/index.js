2905adaa556c2f8129d11fc2c878b5412be08420
dubisdev <dubisdev@gmail.com>
2022-05-06 23:31:31 +0200

    traductions: advisory about natural syntax support

--- src/components/NewTodayTask/index.js
+++ src/components/NewTodayTask/index.js
@@ -2,17 +2,13 @@ import styles from "./styles.module.css";
 import lang from "../../lang";
 
 const NewTaskInterface = () => {
-	const strings = lang.NewTodayTask;
-	return (
-		<div className={styles.wrapper}>
-			<h2>{strings.title}</h2>
-			{strings.description}
-			<ul>
-				<li>{strings.li1}</li>
-				<li>{strings.li2}</li>
-			</ul>
-		</div>
-	);
+  const strings = lang.NewTodayTask;
+  return (
+    <div className={styles.wrapper}>
+      <h2>{strings.title}</h2>
+      {strings.description}
+    </div>
+  );
 };
 
 export default NewTaskInterface;
